ed02a29295a24376902218da271cdd426a85affa
Isabel Granados <104743185+i-greb@users.noreply.github.com>
2023-12-05 09:08:46 -0600

    se docomentó

--- app/Filters/ApiFilter.php
+++ app/Filters/ApiFilter.php
@@ -3,10 +3,15 @@ namespace App\Filters;
 use Illuminate\Http\Request;
 
 class ApiFilter{
+    /*almacena los parámetros seguros*/
     protected $safeParams = [];
+    /*almacena el mapeo de columnas*/
     protected $columnMap = [];
+    /*almacena el mapeo de operadores*/
     protected $operatorMap = [];
 
+    /*se crea un método para transformar la solicitud del filtro, permitiendo el acceso
+    a los parámetros de la URL, datos del formulario,etc*/
     public function transform(Request $request){
         $eloQuery = [];
         foreach($this->safeParams as $parm => $operators){
